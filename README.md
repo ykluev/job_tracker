8fe01ff07b06d4fdeb344772cc63cbae1a2f5188
Yulia Tamara Kluev <ytkluev@uwaterloo.ca>
2025-05-15 12:50:09 -0400

    init

---
+++ README.md
@@ -0,0 +1,49 @@
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Expanding the ESLint configuration
+
+If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+
+
+# Requirements
+
+Users Should...
+
+- be able to set a status for each job app
+- be able to filter by 
+    - status
+    - date
+- be able to search
+- be able to click into a view of the job to add notes
+- configure url filters
+- export to cvs?
+
+
+Job
+
+- createdAt
+- status
+    - cold apply
+    - referred
+    - interview
+    - rejected
+    - offer
+- notes
+- 
+
+
+
+TODO: 
+
+- update job object with status
+- add a filter button
+    - filter by status
+    
---
+++ src/App.jsx
@@ -0,0 +1,117 @@
+import { useState } from 'react'
+import  './App.css'
+
+
+const App = () => {
+  const start_jobs = [
+    {
+      id:1,
+      company: "Gem",
+      name: "SWE 1",
+      url: "www.linkedin.com",
+      status: "NA"
+
+    },
+    {
+      id:2,
+      company: "Borq",
+      name: "Software Engineer",
+      url: "www.linkedin.com",
+      status: "NA"
+    }, 
+    {
+      id:3,
+      company: "Zip",
+      name: "Junior SWE",
+      url: "www.linkedin.com",
+      status: "NA"
+    }
+  ]
+
+  const [jobs, setJobs] = useState(start_jobs)
+  const [newJobUrl, setNewJobUrl] = useState('')
+
+  const handleJobUrlChange = (event) => {
+    console.log(event.target.value)
+    setNewJobUrl(event.target.value)
+  }
+
+
+  const handleJobStatusChange = (id, event) => {
+    event.preventDefault()
+    console.log(event)
+    console.log(id)
+    
+    const updated_jobs = jobs.map( j  => {
+      if (j.id === id) {
+        console.log("HERE")
+        j.status = event.target.value
+      }
+    })
+
+    setJobs(updated_jobs);
+  }
+
+
+  const addJob = (event) => {
+    event.preventDefault()
+    console.log("button clicked", event.target)
+    const jobObject = {
+      id: jobs.length + 1,
+      name: 'SWE',
+      company : "lame",
+      url: newJobUrl,
+      status: "NA"
+    }
+
+    setJobs(jobs.concat(jobObject))
+    setNewJobUrl('')
+  }
+
+  return (
+    <>
+      <h1>Job Tracker</h1>
+      <form onSubmit={addJob}>
+        <input
+          className='linkedin-input'
+          placeholder='LinkedIn URL...'
+          value={newJobUrl}
+          onChange={handleJobUrlChange}
+        />
+        <button type='submit' className='submit-btn'>save</button>
+      </form>
+
+      <table className='job-applications'>
+        <tbody>
+          <tr>
+            <th>Company</th>
+            <th>Position</th>
+            <th>Job URL</th>
+            <th>Status</th>
+
+          </tr>
+          {
+            jobs.map(job =>
+              <tr key={job.id}>
+                <td>{job.company}</td>
+                <td>{job.name}</td>
+                <td><a href="url">{job.url}</a></td>
+                <td>
+                  <select name="status" className='app-status' onChange={(e) => handleJobStatusChange(job.id, e)}>
+                      <option value="NA">N/A</option>
+                      <option value="pending">Pending</option>
+                      <option value="referred">Referred</option>
+                      <option value="applied">Applied</option>
+                      <option value="rejected">Rejected</option>
+                    </select>
+                </td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default App
